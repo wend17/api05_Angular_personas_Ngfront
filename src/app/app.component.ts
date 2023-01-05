9cf898299c883963126060ab5875a11a756bec6b
Wendy <wendyvillazana@gmail.com>
2023-01-05 17:15:37 -0500

    appcomponentts configurando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import {EstadosService} from './services/estados/estados.service';
+import {PaisesService} from './services/paises/paises.service';
+import {PersonaService} from './services/persona/persona.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'persona_front';
+export class AppComponent implements OnInit{
+  
+  personaForm ?: FormGroup;
+
+  constructor(
+    public fb:FormBuilder,
+    public estadosService: EstadosService,
+    public paisesService: PaisesService,
+    public personaService: PersonaService
+
+  ){
+
+  }
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({
+        nombre : ['',Validators.required],
+        apellido : ['',Validators.required],
+        edad : ['',Validators.required],
+        pais : ['',Validators.required],
+        estado : ['',Validators.required],
+    })
+  }
 }

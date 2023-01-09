319c578b288bd479615e68052f5b555957c66352
Wendy <wendyvillazana@gmail.com>
2023-01-09 16:33:44 -0500

    serv persona y fun eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { EstadosService } from './services/estados/estados.service';
-import { PaisesService } from './services/paises/paises.service';
-import { PersonaService } from './services/persona/persona.service';
-
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {EstadosService} from './services/estados/estados.service';
+import {PaisesService} from './services/paises/paises.service';
+import {PersonaService} from './services/persona/persona.service';
 
 
 @Component({
@@ -16,17 +15,18 @@ export class AppComponent implements OnInit {
   personaForm!: FormGroup;
   paises: any;
   estados: any;
- 
+  personas: any;
+
 
   constructor(
     public fb: FormBuilder,
     public estadosService: EstadosService,
     public paisesService: PaisesService,
     public personaService: PersonaService
-
   ) {
 
   }
+
   ngOnInit(): void {
     this.personaForm = new FormGroup({
       nombre: new FormControl('', [Validators.required]),
@@ -37,25 +37,61 @@ export class AppComponent implements OnInit {
     })
 
     this.paisesService.getAllPaises().subscribe(resp => {
-      this.paises = resp;
+        this.paises = resp;
+      },
+      error => {
+        console.error(error)
+      }
+    );
 
+    this.personaService.getAllPersonas().subscribe(resp => {
+        this.personas = resp;
+        console.log(resp);
+      },
+      error => {
+        console.error(error)
+      }
+    );
 
-    },
-      error => { console.error(error) }
-    )
-
+    this.personaForm.get('pais')?.valueChanges.subscribe(value => {
+      console.log('busca estados')
+      console.log(value)
+      if(value){
+        this.estadosService.getAllEstadosByPais(value.id).subscribe(resp => {
+            this.estados = resp;
+          },
+          error => {
+            console.error(error)
+          }
+        );
+      }
+    })
 
   }
+
   guardar(): void {
-
+    const request = this.personaForm.value
+    if (!request.estado) {
+      alert('debe ingresar estado')
+      return
+    }
+    this.personaService.savePersona(request).subscribe(resp => {
+        this.personaForm.reset();
+        this.personas.push(resp);
+      },
+      error => {
+        console.error(error)
+      }
+    )
   }
 
-  cargarEstadosPorPaisesId(event:any) {
-  
-    this.estadosService.getAllEstadosByPais(event.target.value).subscribe(resp => {
-      this.estados = resp;
-    },
-      error => { console.error(error) }
-      );
+  eliminar(persona: any) {
+    this.personaService.deletePersona(persona.id).subscribe(resp => {
+      console.log(resp)
+      if (resp === true) {
+        this.personas.pop(persona)
+      }
+    })
   }
+
 }
--- src/app/services/persona/persona.service.ts
+++ src/app/services/persona/persona.service.ts
@@ -7,11 +7,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PersonaService {
 
- 
+  private API_SERVER = "http://localhost:8080/personas";
 
   constructor(
-   
+    private httpClient: HttpClient
   ) { }
 
-   
+  public getAllPersonas():Observable<any>{
+    return this.httpClient.get(this.API_SERVER);
+  }
+
+  public savePersona (persona:any):Observable<any>{
+    return this.httpClient.post(this.API_SERVER,persona)
+  }
+
+  public deletePersona(id:any):Observable<any>{
+    return this.httpClient.delete(this.API_SERVER + "/delete/" + id);
+  }
 }
